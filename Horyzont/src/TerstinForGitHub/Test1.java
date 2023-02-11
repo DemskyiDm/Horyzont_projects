8891395f07d5d185661f787d13cff67d9c762020
Dmytro Demskyi <demskyidm@gmail.com>
2023-02-11 16:48:13 +0100

    author

--- Horyzont/src/TerstinForGitHub/Test1.java
+++ Horyzont/src/TerstinForGitHub/Test1.java
@@ -1,2 +1,17 @@
-package TerstinForGitHub;public class Test1 {
+package TerstinForGitHub;
+
+import static Petle.zad4.podzPrzez5;
+
+public class Test1 {
+    public static void main(String[] args) {
+        podzPrzez5();
+        podzPrzez5();
+        uruchomsilnik();
+        podzPrzez5();
+    }
+
+    private static void uruchomsilnik() {
+    }private static void uruchomsilnik1() {
+    }private static void uruchomsilnik2() {
+    }
 }
--- Horyzont/src/Test5.java
+++ Horyzont/src/Test5.java
@@ -1,7 +1,7 @@
-package Kolekcje;
-
 public class Test5 {
     public static void main(String[] args) {
-        System.out.println("dsfdsf");
+
     }
+
 }
+
